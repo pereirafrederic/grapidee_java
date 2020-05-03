64ee0cc401c9204da3086671b0284e128d90ddee
pereira frédéric <pereira.frederic.developpeur@gmail.com>
2020-05-03 18:09:16 +0200

    Update V1__Initial_base.sql

--- src/main/resources/db/migration/V1__Initial_base.sql
+++ src/main/resources/db/migration/V1__Initial_base.sql
@@ -1,68 +1,56 @@
+CREATE TABLE "public"."idee"
+(
+   id int PRIMARY KEY NOT NULL,
+   nom varchar(100) NOT NULL,
+   description varchar(1000) DEFAULT NULL::character varying
+)
+;
+CREATE UNIQUE INDEX idee_pkey ON "public"."idee"(id)
+;
 
-CREATE TABLE asso_grappe (
-  id bigint(20) NOT NULL,
-  idee_id bigint(20) NOT NULL,
-  grappe_id bigint(20) NOT NULL,
-  type_id bigint(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE "public"."type_grappe"
+(
+   id int PRIMARY KEY NOT NULL,
+   nom varchar(100) NOT NULL
+)
+;
+CREATE UNIQUE INDEX type_grappe_pkey ON "public"."type_grappe"(id)
+;
 
 
-CREATE TABLE asso_idee (
-  id bigint(20) NOT NULL,
-  liaison varchar(100) COLLATE utf8_bin DEFAULT NULL,
-  maitre_id bigint(20) NOT NULL,
-  esclave_id bigint(20) NOT NULL,
-  grappe_id bigint(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE "public"."grappe"
+(
+   id int PRIMARY KEY NOT NULL,
+   nom varchar(100) NOT NULL,
+   description varchar(1000) DEFAULT NULL::character varying,
+   grappe_id bigint,
+   type_id bigint,
+   ordre int DEFAULT 99
+)
+;
+CREATE UNIQUE INDEX grappe_pkey ON "public"."grappe"(id)
+;
 
 
-CREATE TABLE grappe (
-  id bigint(20) NOT NULL,
-  nom varchar(100) COLLATE utf8_bin NOT NULL,
-  description varchar(1000) COLLATE utf8_bin DEFAULT NULL,
-  grappe_id bigint(20) DEFAULT NULL,
-  type_id bigint(11) DEFAULT NULL,
-  ordre int(11) DEFAULT '99'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE "public"."asso_grappe"
+(
+   id int PRIMARY KEY NOT NULL,
+   idee_id bigint NOT NULL,
+   grappe_id bigint NOT NULL,
+   type_id bigint
+)
+;
+CREATE UNIQUE INDEX asso_grappe_pkey ON "public"."asso_grappe"(id)
+;
+CREATE TABLE "public"."asso_idee"
+(
+   id int PRIMARY KEY NOT NULL,
+   liaison varchar(100) DEFAULT NULL::character varying,
+   maitre_id bigint NOT NULL,
+   esclave_id bigint NOT NULL,
+   grappe_id bigint
+)
+;
+CREATE UNIQUE INDEX asso_idee_pkey ON "public"."asso_idee"(id)
+;
 
-CREATE TABLE idee (
-  id bigint(20) NOT NULL,
-  nom varchar(100) COLLATE utf8_bin NOT NULL,
-  description longtext COLLATE utf8_bin
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-CREATE TABLE type_grappe (
-  id bigint(20) NOT NULL,
-  nom varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-ALTER TABLE asso_grappe
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE asso_idee
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE grappe
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE idee
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE type_grappe
-  ADD PRIMARY KEY (id);
-
-ALTER TABLE asso_grappe
-  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=472;
-
-ALTER TABLE asso_idee
-  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=515;
-
-ALTER TABLE grappe
-  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=684;
-
-ALTER TABLE idee
-  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1756;
-
-ALTER TABLE type_grappe
-  MODIFY id bigint(20) NOT NULL AUTO_INCREMENT;
-COMMIT;
